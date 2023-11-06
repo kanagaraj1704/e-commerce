8c4f6f3d6b191034c1a01cc9ed21779907c35edc
kanagaraj <kanagaraj@nanonino.in>
2023-11-06 13:25:55 +0530

    add cart finish

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Home from './component/Home';
 import Products from './component/Products';
 import Product from './component/Product';
 import Cart from './component/Cart';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Checkout from './component/Checkout';
 
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/Products" element={<Products/>}/>
-        <Route path="/Cart" element={<Cart/>}/>
-        <Route path="/Products/:id" element={<Product/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/Products/:id" element={<Product />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </>
   );
--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector} from 'react-redux';
+// import { NavLink } from 'react-router-dom';
+// import {delCart } from '../redux/action';
 import { NavLink } from 'react-router-dom';
-import { addCart,delCart } from '../redux/action';
 
 const Cart = () => {
-  const state =useSelector((state)=>state.hadleCart);
-  const dispatch =useDispatch;
+  const state =useSelector((state)=> state.handleCart);
+  console.log(123,state);
+  // const dispatch =useDispatch;  
 
-  const handleAdd =(item) =>{
-    dispatch(addCart(item));
-  };
-  const handleDel =(item) =>{
-    dispatch(delCart(item));
-  };
+  // const handleAdd =(item) =>{
+  //   dispatch(addCart(item));
+  // };
+  // const handleDel =(item) =>{
+  //   dispatch(delCart(item));
+  // };
 
   const emptyCart = () =>{
     return(
@@ -25,16 +27,42 @@ const Cart = () => {
     </div>
     );
   };
-  const cartItems =(product) => {
+  const cartItems = (cartItem) => {
+    console.log(cartItems);
     return(
-      <>
-      <div className="px-4 my-5 bg-light rounded-3 py-5">
-        
+        <div className="px-4 my-5 bg-light rounded-3" key={cartItem.id}>
+            <div className="container py-4">
+                <div className="row justify-content-center">
+                    <div className="col-md-4">
+                        <img src={cartItem.image} alt={cartItem.title} height="200px" width="180px" />
+                    </div>
+                    <div className="col-md-4">
+                        <h3>{cartItem.title}</h3>
+                        <p className="lead fw-bold">${cartItem.price}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+const button = () => {
+  return(
+      <div className="container">
+          <div className="row">
+              <NavLink to="/checkout" className="btn btn-outline-primary mb-5 w-25 mx-auto">Proceed To checkout</NavLink>
+          </div>
+
       </div>
+  );
+}
+
+  return(
+     <>
+            {state.length === 0 && emptyCart()}
+            {state.length !== 0 && state.map(cartItems)}
+            {state.length !== 0 && button()}
       </>
-    )
-  }
-  
+  )
   
 }
 
